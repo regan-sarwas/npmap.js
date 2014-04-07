366240b0544bac855737e19e27d2102f1b76f432
Nate Irwin <nate@nateirwin.net>
2014-04-07 09:32:52 -0600

    Clean up

--- src/util/base64.js
+++ src/util/base64.js
@@ -15,9 +15,3 @@ module.exports = (function() {
     }
   };
 })();
-
-
-
-/*
-(function(){function k(f){var a,b,d,e,c,h;d=f.length;b=0;for(a="";b<d;){e=f.charCodeAt(b++)&255;if(b==d){a+=g.charAt(e>>2);a+=g.charAt((e&3)<<4);a+="==";break}c=f.charCodeAt(b++);if(b==d){a+=g.charAt(e>>2);a+=g.charAt((e&3)<<4|(c&240)>>4);a+=g.charAt((c&15)<<2);a+="=";break}h=f.charCodeAt(b++);a+=g.charAt(e>>2);a+=g.charAt((e&3)<<4|(c&240)>>4);a+=g.charAt((c&15)<<2|(h&192)>>6);a+=g.charAt(h&63)}return a}function l(f){var a,b,d,e,c;e=f.length;d=0;for(c="";d<e;){do a=h[f.charCodeAt(d++)&255];while(d<e&&-1==a);if(-1==a)break;do b=h[f.charCodeAt(d++)&255];while(d<e&&-1==b);if(-1==b)break;c+=String.fromCharCode(a<<2|(b&48)>>4);do{a=f.charCodeAt(d++)&255;if(61==a)return c;a=h[a]}while(d<e&&-1==a);if(-1==a)break;c+=String.fromCharCode((b&15)<<4|(a&60)>>2);do{b=f.charCodeAt(d++)&255;if(61==b)return c;b=h[b]}while(d<e&&-1==b);if(-1==b)break;c+=String.fromCharCode((a&3)<<6|b)}return c}var g="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",h=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-1,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1];window.btoa||(window.btoa=k);window.atob||(window.atob=l)})();
-*/
