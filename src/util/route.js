3b55c72f8d7cc396ffaaa4a479bcc731bde82f9b
Nate Irwin <nate@nateirwin.net>
2014-04-28 13:35:36 -0600

    More routing/directions work

--- src/util/route.js
+++ src/util/route.js
@@ -29,7 +29,7 @@ module.exports = ({
             });
           },
           success: function(response) {
-            console.log(response);
+            //console.log(response);
             callback(response);
           },
           type: 'jsonp',
