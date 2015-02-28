d5ea7e94e21c97e229729c49af6cbe954177e08b
Nate Irwin <nate@nateirwin.net>
2015-02-28 15:55:55 -0700

    Copy and uglify plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,52 +96,6 @@ module.exports = function(grunt) {
         ]
       }
     },
-    compress: {
-      production: {
-        options: {
-          mode: 'gzip'
-        },
-        files: [{
-          cwd: 'dist/',
-          dest: 'dist/gzip/',
-          expand: true,
-          ext: '.css',
-          src: [
-            'npmap-standalone.css',
-            'npmap.css'
-          ]
-        },{
-          cwd: 'dist/',
-          dest: 'dist/gzip/',
-          expand: true,
-          ext: '.min.css',
-          src: [
-            'npmap-standalone.min.css',
-            'npmap.min.css'
-          ]
-        },{
-          cwd: 'dist/',
-          dest: 'dist/gzip/',
-          expand: true,
-          ext: '.js',
-          src: [
-            'npmap-bootstrap.js',
-            'npmap-standalone.js',
-            'npmap.js'
-          ]
-        },{
-          cwd: 'dist/',
-          dest: 'dist/gzip/',
-          expand: true,
-          ext: '.min.js',
-          src: [
-            'npmap-bootstrap.min.js',
-            'npmap-standalone.min.js',
-            'npmap.min.js'
-          ]
-        }]
-      }
-    },
     concat: {
       css: {
         dest: 'dist/npmap.css',
@@ -195,6 +149,10 @@ module.exports = function(grunt) {
         src: [
           '**/*'
         ]
+      },
+      plugins: {
+        dest: 'dist/',
+        src: 'plugins/**/*'
       }
     },
     csslint: {
@@ -241,17 +199,12 @@ module.exports = function(grunt) {
     },
     pkg: pkg,
     uglify: {
-      npmap: {
-        dest: 'dist/npmap.min.js',
-        src: 'dist/npmap.js'
-      },
-      'npmap-bootstrap': {
-        dest: 'dist/npmap-bootstrap.min.js',
-        src: 'dist/npmap-bootstrap.js'
-      },
-      'npmap-standalone': {
-        dest: 'dist/npmap-standalone.min.js',
-        src: 'dist/npmap-standalone.js'
+      all: {
+        cwd: 'dist/',
+        expand: true,
+        dest: 'dist/',
+        ext: '.min.js',
+        src: ['**/*.js', '!*.min.js']
       }
     },
     usebanner: {
@@ -271,7 +224,7 @@ module.exports = function(grunt) {
   });
 
   loadNpmTasks();
-  grunt.registerTask('build', ['clean:dist', 'copy:css', 'copy:examples', 'copy:images', 'copy:javascript', 'copy:npmaki', 'concat', 'browserify', 'uglify', 'cssmin', 'usebanner']); //TODO: csscomb, validation
+  grunt.registerTask('build', ['clean:dist', 'copy:css', 'copy:examples', 'copy:images', 'copy:javascript', 'copy:npmaki', 'copy:plugins', 'concat', 'browserify', 'uglify', 'cssmin', 'usebanner']); //TODO: csscomb, validation
   grunt.registerTask('deploy', ['clean:nps', 'mkdir:nps', 'copy:nps', 'akamai_rest_purge', 'http']);
   grunt.registerTask('lint', ['csslint']); //TODO: jshint
   grunt.registerTask('test', ['mocha_phantomjs']);
