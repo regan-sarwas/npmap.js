2c96fbf420aab45955634594a4b08dc4db75d8b0
Nate Irwin <nate@nateirwin.net>
2015-03-01 17:36:59 -0700

    Cleanup

--- src/control/edit.js
+++ src/control/edit.js
@@ -89,6 +89,7 @@ var EditControl = L.Control.extend({
       this._initializeMode(container, new L.Draw.Circle(map, options.circle), 'Draw a circle');
     }
 
+    this._map = map;
     this._featureGroup.on('click', function(e) {
       var editing = e.layer.editing,
         leafletId;
@@ -101,7 +102,7 @@ var EditControl = L.Control.extend({
         }
 
         if (editId === leafletId) {
-          e.layer.editing.disable();
+          editing.disable();
           editId = null;
           editShape = null;
         } else {
@@ -109,7 +110,7 @@ var EditControl = L.Control.extend({
             editShape.editing.disable();
           }
 
-          e.layer.editing.enable();
+          editing.enable();
           editId = leafletId;
           editShape = e.layer;
         }
@@ -121,38 +122,38 @@ var EditControl = L.Control.extend({
         }
       }
     });
-    this._map = map;
-    map.addLayer(this._featureGroup);
-    map.on('click', function() {
-      if (editShape) {
-        editShape.editing.disable();
-        editId = null;
-        editShape = null;
-      }
-    });
-    map.on('draw:created', function(e) {
-      if (me._activeMode) {
-        me._featureGroup.addLayer(e.layer);
-
-        if (e.layerType === 'marker') {
-          e.layer.dragging.enable();
-          e.layer.on('dragstart', function() {
-            if (editShape) {
-              editShape.editing.disable();
-              editId = null;
-              editShape = null;
-            }
-          });
+    map
+      .addLayer(this._featureGroup)
+      .on('click', function() {
+        if (editShape) {
+          editShape.editing.disable();
+          editId = null;
+          editShape = null;
         }
-      }
-    });
-    map.on('draw:drawstart', function() {
-      if (editShape) {
-        editShape.editing.disable();
-        editId = null;
-        editShape = null;
-      }
-    });
+      })
+      .on('draw:created', function(e) {
+        if (me._activeMode) {
+          me._featureGroup.addLayer(e.layer);
+
+          if (e.layerType === 'marker') {
+            e.layer.dragging.enable();
+            e.layer.on('dragstart', function() {
+              if (editShape) {
+                editShape.editing.disable();
+                editId = null;
+                editShape = null;
+              }
+            });
+          }
+        }
+      })
+      .on('draw:drawstart', function() {
+        if (editShape) {
+          editShape.editing.disable();
+          editId = null;
+          editShape = null;
+        }
+      });
 
     return container;
   },
