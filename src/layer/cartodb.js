3b7f32c49bd5b3513b5bcb139f6eefaf107b10c3
MappingKat <katrina@engelsted.co>
2015-03-11 14:05:32 -0700

    removing post from two call and adding a cartodb-post example

--- src/layer/cartodb.js
+++ src/layer/cartodb.js
@@ -55,7 +55,6 @@ var CartoDbLayer = L.TileLayer.extend({
         me.fire('error', error);
         me.errorFired = error;
       },
-      method: 'post',
       success: function(response) {
         var layer = {
           options: {},
@@ -113,7 +112,6 @@ var CartoDbLayer = L.TileLayer.extend({
             error.message = JSON.parse(error.response).error[0];
             me.fire('error', error);
           },
-          method: 'post',
           success: function(response) {
             if (response && response.success && response.data) {
               var root = window.location.protocol + '//' + '{s}.' + response.data.cdn_url[window.location.protocol.replace(':', '')] + '/' + me.options.user + '/api/v1/map/' + response.data.layergroupid,
