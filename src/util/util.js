9338ed5c54db9e8e572d51a509dc25023b5b4ba1
Nate Irwin <nate@nateirwin.net>
2015-08-23 09:38:22 -0600

    Implement semistandard and get rid of a few shims that are no longer needed

--- src/util/util.js
+++ src/util/util.js
@@ -40,80 +40,9 @@ handlebars.registerHelper('toUpperCase', function (str) {
   return str.toUpperCase();
 });
 
-// Shim for Array.indexOf
-if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function (searchElement, fromIndex) {
-    if (this === undefined || this === null) {
-      throw new TypeError('"this" is null or not defined');
-    }
-
-    var length = this.length >>> 0;
-
-    fromIndex = +fromIndex || 0;
-
-    if (Math.abs(fromIndex) === Infinity) {
-      fromIndex = 0;
-    }
-
-    if (fromIndex < 0) {
-      fromIndex += length;
-      if (fromIndex < 0) {
-        fromIndex = 0;
-      }
-    }
-
-    for (; fromIndex < length; fromIndex++) {
-      if (this[fromIndex] === searchElement) {
-        return fromIndex;
-      }
-    }
-
-    return -1;
-  };
-}
-
-// Shim for Array.map because Internet Explorer 8 doesn't support it.
-if (!Array.prototype.map) {
-  Array.prototype.map = function (callback, thisArg) {
-    var T, A, k;
-
-    if (this == null) {
-      throw new TypeError(' this is null or not defined');
-    }
-
-    var O = Object(this);
-    var len = O.length >>> 0;
-
-    if (typeof callback !== 'function') {
-      throw new TypeError(callback + ' is not a function');
-    }
-
-    if (arguments.length > 1) {
-      T = thisArg;
-    }
-
-    A = new Array(len);
-    k = 0;
-
-    while (k < len) {
-      var kValue, mappedValue;
-
-      if (k in O) {
-        kValue = O[k];
-        mappedValue = callback.call(T, kValue, k, O);
-        A[k] = mappedValue;
-      }
-
-      k++;
-    }
-
-    return A;
-  };
-}
-
-// Shim for window.atob/window.btoa.
+// Shim for window.atob/window.btoa. Needed for IE9 support.
 (function () {
-  var decodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+  var decodeChars = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1];
   var encodeChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
   function base64decode (str) {
@@ -126,7 +55,7 @@ if (!Array.prototype.map) {
     while (i < len) {
       do {
         c1 = decodeChars[str.charCodeAt(i++) & 0xff];
-      } while(i < len && c1 === -1);
+      } while (i < len && c1 === -1);
 
       if (c1 === -1) {
         break;
@@ -134,7 +63,7 @@ if (!Array.prototype.map) {
 
       do {
         c2 = decodeChars[str.charCodeAt(i++) & 0xff];
-      } while(i < len && c2 === -1);
+      } while (i < len && c2 === -1);
 
       if (c2 === -1) {
         break;
@@ -199,7 +128,7 @@ if (!Array.prototype.map) {
 
       if (i === len) {
         out += encodeChars.charAt(c1 >> 2);
-        out += encodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+        out += encodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
         out += encodeChars.charAt((c2 & 0xF) << 2);
         out += '=';
         break;
@@ -207,8 +136,8 @@ if (!Array.prototype.map) {
 
       c3 = str.charCodeAt(i++);
       out += encodeChars.charAt(c1 >> 2);
-      out += encodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-      out += encodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+      out += encodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+      out += encodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
       out += encodeChars.charAt(c3 & 0x3F);
     }
 
@@ -244,6 +173,8 @@ module.exports = {
         success: function (response) {
           if (response && response.success) {
             callback(true);
+          } else {
+            callback(false);
           }
         },
         type: 'json',
@@ -262,8 +193,7 @@ module.exports = {
         } else {
           time++;
 
-          // Time out at 1.5 seconds.
-          if (time >= 15) {
+          if (time >= 1500) {
             done = true;
           }
         }
@@ -326,10 +256,10 @@ module.exports = {
     }
   },
   _lazyLoader: require('./lazyloader.js'),
-  _parseLocalUrl: function(url) {
-    return url.replace(location.origin, '');
+  _parseLocalUrl: function (url) {
+    return url.replace(window.location.origin, '');
   },
-  appendCssFile: function(urls, callback) {
+  appendCssFile: function (urls, callback) {
     if (typeof urls === 'string') {
       urls = [
         urls
@@ -338,7 +268,7 @@ module.exports = {
 
     this._lazyLoader(urls, callback);
   },
-  appendJsFile: function(urls, callback) {
+  appendJsFile: function (urls, callback) {
     if (typeof urls === 'string') {
       urls = [
         urls
@@ -347,7 +277,7 @@ module.exports = {
 
     this._lazyLoader(urls, callback);
   },
-  buildUrl: function(base, params) {
+  buildUrl: function (base, params) {
     var returnArray = [];
 
     if (params) {
@@ -366,7 +296,7 @@ module.exports = {
     returnArray.pop();
     return returnArray.join('');
   },
-  cancelEvent: function(e) {
+  cancelEvent: function (e) {
     e = e || window.event;
 
     if (e.preventDefault) {
@@ -375,11 +305,11 @@ module.exports = {
 
     e.returnValue = false;
   },
-  clone: function(obj) {
+  clone: function (obj) {
     // One problem with this: http://stackoverflow.com/questions/11491938/issues-with-date-when-using-json-stringify-and-json-parse/11491993#11491993.
     return JSON.parse(JSON.stringify(obj));
   },
-  dataToList: function(data, fields) {
+  dataToList: function (data, fields) {
     var dl = document.createElement('dl');
 
     for (var prop in data) {
@@ -392,8 +322,8 @@ module.exports = {
       }
 
       if (add) {
-        var dd = document.createElement('dd'),
-          dt = document.createElement('dt');
+        var dd = document.createElement('dd');
+        var dt = document.createElement('dt');
 
         dt.innerHTML = prop;
         dd.innerHTML = data[prop];
@@ -405,10 +335,11 @@ module.exports = {
     return dl;
   },
   // TODO: Needs a lot of cleanup, and also need to document fields option.
-  dataToTable: function(data, fields) {
-    var table = document.createElement('table'),
-      tableBody = document.createElement('tbody'),
-      field, fieldTitles;
+  dataToTable: function (data, fields) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+    var field;
+    var fieldTitles;
 
     table.appendChild(tableBody);
 
@@ -418,7 +349,7 @@ module.exports = {
       for (var i = 0; i < fields.length; i++) {
         field = fields[i];
 
-        if (typeof(field) === 'string') {
+        if (typeof field === 'string') {
           fieldTitles[field] = {
             'title': field
           };
@@ -443,9 +374,9 @@ module.exports = {
       }
 
       if (add) {
-        var tdProperty = document.createElement('td'),
-          tdValue = document.createElement('td'),
-          tr = document.createElement('tr');
+        var tdProperty = document.createElement('td');
+        var tdValue = document.createElement('td');
+        var tr = document.createElement('tr');
 
         if (fieldTitles) {
           tdProperty.innerHTML = fieldTitles[prop].title;
@@ -471,7 +402,7 @@ module.exports = {
 
     return table;
   },
-  escapeHtml: function(unsafe) {
+  escapeHtml: function (unsafe) {
     return unsafe
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
@@ -479,11 +410,11 @@ module.exports = {
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&#039;');
   },
-  getChildElementsByClassName: function(parentNode, className) {
-    var children = parentNode.childNodes,
-      matches = [];
+  getChildElementsByClassName: function (parentNode, className) {
+    var children = parentNode.childNodes;
+    var matches = [];
 
-    function recurse(el) {
+    function recurse (el) {
       var grandChildren = el.children;
 
       if (typeof el.className === 'string' && el.className.indexOf(className) !== -1) {
@@ -510,13 +441,13 @@ module.exports = {
 
     return matches;
   },
-  getChildElementsByNodeName: function(parentNode, nodeName) {
-    var children = parentNode.childNodes,
-      matches = [];
+  getChildElementsByNodeName: function (parentNode, nodeName) {
+    var children = parentNode.childNodes;
+    var matches = [];
 
     nodeName = nodeName.toLowerCase();
 
-    function recurse(el) {
+    function recurse (el) {
       var grandChildren = el.children;
 
       if (typeof el.nodeName === 'string' && el.nodeName.toLowerCase() === nodeName) {
@@ -536,10 +467,10 @@ module.exports = {
 
     return matches;
   },
-  getElementsByClassName: function(className) {
-    var matches = [],
-      regex = new RegExp('(^|\\s)' + className + '(\\s|$)'),
-      tmp = document.getElementsByTagName('*');
+  getElementsByClassName: function (className) {
+    var matches = [];
+    var regex = new RegExp('(^|\\s)' + className + '(\\s|$)');
+    var tmp = document.getElementsByTagName('*');
 
     for (var i = 0; i < tmp.length; i++) {
       if (regex.test(tmp[i].className)) {
@@ -549,14 +480,14 @@ module.exports = {
 
     return matches;
   },
-  getEventObject: function(e) {
+  getEventObject: function (e) {
     if (!e) {
       e = window.event;
     }
 
     return e;
   },
-  getEventObjectTarget: function(e) {
+  getEventObjectTarget: function (e) {
     var target;
 
     if (e.target) {
@@ -571,14 +502,14 @@ module.exports = {
 
     return target;
   },
-  getNextSibling: function(el) {
+  getNextSibling: function (el) {
     do {
       el = el.nextSibling;
     } while (el && el.nodeType !== 1);
 
     return el;
   },
-  getOffset: function(el) {
+  getOffset: function (el) {
     for (var lx = 0, ly = 0; el !== null; lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
 
     return {
@@ -586,13 +517,13 @@ module.exports = {
       top: ly
     };
   },
-  getOuterDimensions: function(el) {
-    var height = 0,
-      width = 0;
+  getOuterDimensions: function (el) {
+    var height = 0;
+    var width = 0;
 
     if (el) {
-      var changed = [],
-        parentNode = el.parentNode;
+      var changed = [];
+      var parentNode = el.parentNode;
 
       this._checkDisplay(el, changed);
 
@@ -623,13 +554,14 @@ module.exports = {
       width: width
     };
   },
-  getOuterHtml: function(el) {
+  getOuterHtml: function (el) {
     if (!el || !el.tagName) {
       return '';
     }
 
-    var div = document.createElement('div'),
-      ax, txt;
+    var div = document.createElement('div');
+    var ax;
+    var txt;
 
     div.appendChild(el.cloneNode(false));
     txt = div.innerHTML;
@@ -638,30 +570,30 @@ module.exports = {
     div = null;
     return txt;
   },
-  getPosition: function(el) {
+  getPosition: function (el) {
     var obj = {
       left: 0,
       top: 0
-    },
-      offset = this.getOffset(el),
-      offsetParent = this.getOffset(el.parentNode);
+    };
+    var offset = this.getOffset(el);
+    var offsetParent = this.getOffset(el.parentNode);
 
     obj.left = offset.left - offsetParent.left;
     obj.top = offset.top - offsetParent.top;
 
     return obj;
   },
-  getPreviousSibling: function(el) {
+  getPreviousSibling: function (el) {
     do {
       el = el.previousSibling;
     } while (el && el.nodeType !== 1);
 
     return el;
   },
-  getPropertyCount: function(obj) {
+  getPropertyCount: function (obj) {
     if (!Object.keys) {
-      var keys = [],
-        k;
+      var keys = [];
+      var k;
 
       for (k in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, k)) {
@@ -674,37 +606,37 @@ module.exports = {
       return Object.keys(obj).length;
     }
   },
-  handlebars: function(template, data) {
+  handlebars: function (template, data) {
     template = handlebars.compile(template);
 
     return template(data);
   },
-  isHidden: function(el) {
+  isHidden: function (el) {
     return (el.offsetParent === null);
   },
-  isLocalUrl: function(url) {
-    if (url.indexOf(location.origin) === 0) {
+  isLocalUrl: function (url) {
+    if (url.indexOf(window.location.origin) === 0) {
       return true;
     } else {
       return !(/^(?:[a-z]+:)?\/\//i.test(url));
     }
   },
-  isNumeric: function(val) {
+  isNumeric: function (val) {
     return !isNaN(parseFloat(val)) && isFinite(val);
   },
-  linkify: function(text, shorten, target) {
-    var regexRoot = '\\b(https?:\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[A-Z0-9+&@#/%=~_|])',
-      regexLink = new RegExp(regexRoot, 'gi'),
-      regexShorten = new RegExp('>' + regexRoot +'</a>', 'gi'),
-      textLinked = text.replace(regexLink, '<a href="$1"' + (target ? ' target="' + target + '"' : '') + '>$1</a>');
+  linkify: function (text, shorten, target) {
+    var regexRoot = '\\b(https?:\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[A-Z0-9+&@#/%=~_|])';
+    var regexLink = new RegExp(regexRoot, 'gi');
+    var regexShorten = new RegExp('>' + regexRoot + '</a>', 'gi');
+    var textLinked = text.replace(regexLink, '<a href="$1"' + (target ? ' target="' + target + '"' : '') + '>$1</a>');
 
     if (shorten) {
       var matchArray = textLinked.match(regexShorten);
 
       if (matchArray) {
         for (var i = 0; i < matchArray.length; i++) {
-          var newBase = matchArray[i].substr(1, matchArray[i].length - 5).replace(/https?:\/\//gi, ''),
-            newName = newBase.substr(0, shorten) + (newBase.length > shorten ? '&hellip;' : '');
+          var newBase = matchArray[i].substr(1, matchArray[i].length - 5).replace(/https?:\/\//gi, '');
+          var newName = newBase.substr(0, shorten) + (newBase.length > shorten ? '&hellip;' : '');
 
           if (newBase.length - 1 === shorten) {
             newName = newName.substr(0, shorten) + newBase.substr(shorten, 1);
@@ -796,9 +728,11 @@ module.exports = {
     var previous;
 
     function changeImage (direction) {
-      var lis = ul.childNodes,
-        maxImg = lis.length,
-        curImg, j, li;
+      var lis = ul.childNodes;
+      var maxImg = lis.length;
+      var curImg;
+      var j;
+      var li;
 
       for (j = 0; j < lis.length; j++) {
         li = lis[j];
@@ -835,18 +769,18 @@ module.exports = {
     }
 
     for (var i = 0; i < media.length; i++) {
-      var config = media[i],
-        type = types[config.type];
+      var config = media[i];
+      var type = types[config.type];
 
       if (type) {
         images = type(data[config.id.replace('{{', '').replace('}}', '')]);
 
         for (var k = 0; k < images.length; k++) {
-          var a = document.createElement('a'),
-            image = images[k],
-            img = document.createElement('img'),
-            imgStyles = [],
-            li = document.createElement('li');
+          var a = document.createElement('a');
+          var image = images[k];
+          var img = document.createElement('img');
+          var imgStyles = [];
+          var li = document.createElement('li');
 
           if (typeof config.height === 'number') {
             imgStyles += 'height:' + config.height + 'px;';
@@ -886,10 +820,10 @@ module.exports = {
       next = document.createElement('button');
       next.setAttribute('class', 'btn btn-circle next');
       next.innerHTML = '&gt;';
-      L.DomEvent.addListener(previous, 'click', function() {
+      L.DomEvent.addListener(previous, 'click', function () {
         changeImage(-1);
       });
-      L.DomEvent.addListener(next, 'click', function() {
+      L.DomEvent.addListener(next, 'click', function () {
         changeImage(1);
       });
       buttons.appendChild(previous);
@@ -899,12 +833,12 @@ module.exports = {
 
     return div;
   },
-  parseDomainFromUrl: function(url) {
+  parseDomainFromUrl: function (url) {
     var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
 
     return matches && matches[1];
   },
-  putCursorAtEndOfInput: function(input) {
+  putCursorAtEndOfInput: function (input) {
     if (input.setSelectionRange) {
       var length = input.value.length * 2;
       input.setSelectionRange(length, length);
@@ -913,28 +847,28 @@ module.exports = {
     }
   },
   reqwest: reqwest,
-  strict: function(_, type) {
+  strict: function (_, type) {
     if (typeof _ !== type) {
       throw new Error('Invalid argument: ' + type + ' expected');
     }
   },
-  strictInstance: function(_, klass, name) {
+  strictInstance: function (_, klass, name) {
     if (!(_ instanceof klass)) {
       throw new Error('Invalid argument: ' + name + ' expected');
     }
   },
-  strictOneOf: function(_, values) {
+  strictOneOf: function (_, values) {
     if (values.indexOf(_) === -1) {
       throw new Error('Invalid argument: ' + _ + ' given, valid values are ' + values.join(', '));
     }
   },
-  stripHtml: function(html) {
+  stripHtml: function (html) {
     var tmp = document.createElement('div');
     tmp.innerHTML = html;
     return tmp.textContent || tmp.innerText || '';
   },
   // http://stackoverflow.com/a/7616755/27540
-  supportsCors: function() {
+  supportsCors: function () {
     if ('withCredentials' in new XMLHttpRequest()) {
       return 'yes';
     } else if (typeof XDomainRequest !== 'undefined') {
@@ -943,7 +877,7 @@ module.exports = {
       return 'no';
     }
   },
-  unescapeHtml: function(unsafe) {
+  unescapeHtml: function (unsafe) {
     return unsafe
       .replace(/&amp;/g, '&')
       .replace(/&lt;/g, '<')
