b7e3bfe8fcd945188b2e0bc37118086353b0ed9c
Ryan J. Hodge <ryanjhodge@users.noreply.github.com>
2015-11-13 14:18:47 -0800

    Implement semistandard style

--- src/icon/maki.js
+++ src/icon/maki.js
@@ -1,8 +1,8 @@
 /* global L */
 
-'use strict'
+'use strict';
 
-var util = require('../util/util')
+var util = require('../util/util');
 var MakiIcon = L.Icon.extend({
   options: {
     'marker-color': '#000000',
@@ -12,9 +12,9 @@ var MakiIcon = L.Icon.extend({
     CSS_TEMPLATE: 'url(https://api.mapbox.com/v4/marker/pin-{{size}}{{symbol}}+{{color}}{{retina}}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q)'
   },
   initialize: function (options) {
-    options = options || {}
+    options = options || {};
 
-    var size = options['marker-size'] || 'medium'
+    var size = options['marker-size'] || 'medium';
     var sizes = {
       large: {
         iconAnchor: [17.5, 49],
@@ -31,32 +31,32 @@ var MakiIcon = L.Icon.extend({
         iconSize: [20, 30],
         popupAnchor: [2, -24]
       }
-    }
+    };
 
-    L.Util.extend(options, sizes[size])
-    L.setOptions(this, options)
+    L.Util.extend(options, sizes[size]);
+    L.setOptions(this, options);
   },
   createIcon: function (oldIcon) {
-    var div = (oldIcon && oldIcon.tagName === 'DIV') ? oldIcon : document.createElement('div')
-    var options = this.options
+    var div = (oldIcon && oldIcon.tagName === 'DIV') ? oldIcon : document.createElement('div');
+    var options = this.options;
 
-    this._setIconStyles(div, 'icon')
+    this._setIconStyles(div, 'icon');
     div.style.backgroundImage = util.handlebars(MakiIcon.CSS_TEMPLATE, {
       color: options['marker-color'].replace('#', ''),
       retina: L.Browser.retina ? '@2x' : '',
       size: options['marker-size'].slice(0, 1),
       symbol: options['marker-symbol'] ? '-' + options['marker-symbol'] : ''
-    })
-    return div
+    });
+    return div;
   },
   createShadow: function () {
-    return null
+    return null;
   }
-})
+});
 
 L.Marker.mergeOptions({
   icon: new MakiIcon()
-})
+});
 module.exports = function (options) {
-  return new MakiIcon(options)
-}
+  return new MakiIcon(options);
+};
